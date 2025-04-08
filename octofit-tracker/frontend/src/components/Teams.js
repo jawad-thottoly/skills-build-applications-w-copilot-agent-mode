34735230ec230e7b6e7a0418b34b4e554a1dea4c
jawad-thottoly <jawadthottoly@outlook.com>
2025-04-08 15:22:49 +0000

    step5

---
+++ octofit-tracker/frontend/src/components/Teams.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Teams() {
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    fetch('https://[REPLACE-THIS-WITH-YOUR-CODESPACE-NAME]-8000.app.github.dev/api/teams/')
+      .then(response => response.json())
+      .then(data => setTeams(data))
+      .catch(error => console.error('Error fetching teams:', error));
+  }, []);
+
+  return (
+    <div className="container mt-4">
+      <h1 className="display-4 text-center">Teams</h1>
+      <table className="table table-striped table-bordered mt-4">
+        <thead className="thead-dark">
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Team Name</th>
+            <th scope="col">Members</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teams.map(team => (
+            <tr key={team._id}>
+              <td>{team._id}</td>
+              <td>{team.name}</td>
+              <td>{team.members.join(', ')}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Teams;
